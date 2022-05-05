cabcdc535f4eab97e68e0e978a7f08e190a7f198
vollmersj <s.vollmer.4@warwick.ac.uk>
2022-05-05 08:49:34 +0200

    initial

---
+++ test.jl
@@ -0,0 +1,16 @@
+# following david anthoff 
+
+using VegaLite, VegaDatasets, Query, DataFrames
+
+cars = dataset("cars")
+
+cars |> @select(:Acceleration,:Name) |> collect
+
+function foo(data,origin)
+    df = data |> @filter(_.Origin == origin) |> DataFrame
+    return df |> @vlplot(:point,:Acceleration,:Miles_per_Gallon)
+end
+
+p=foo(cars,"USA")
+
+p |> save("foo.png")
